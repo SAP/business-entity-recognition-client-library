db9778296cd19278a45fca93f71e5774daa23c97
Gokul Mohanarangan <gokul.mohanarangan@sap.com>
2021-06-30 11:11:23 +0530

    configurations for pypi

--- setup.py
+++ setup.py
@@ -7,23 +7,29 @@ https://github.com/pypa/sampleproject
 
 # Always prefer setuptools over distutils
 from codecs import open
-
 from setuptools import setup, find_packages
 
-
-def get_version():
-    with open('version.txt') as ver_file:
-        version_str = ver_file.readline().rstrip()
-    return version_str
-
-
-setup(name='business-entity-recognition',
-      version=get_version(),
+setup(name='business-entity-recognition-client-library',
+      version=1.5,
+      license='apache-2.0',
+      install_requires=[
+          'requests==2.25.1'
+      ],
       packages=find_packages(
           exclude=['examples*']),
       description='Exposes easy consumable methods via a client library, to access and use the API offerings of the AI '
                   'BUS Service - Business Entity Recognition.',
-      author_email='komal.narsinghani@sap.com',
-      maintainer='DL ML Apps BLR Prasanna DR',
-      maintainer_email='DL_5CEDF333B4A5864B89BE0EE5@global.corp.sap'
+      author='Gokul Mohanarangan',
+      author_email='gokumohan@gmail.com',
+      url='https://github.com/SAP/business-entity-recognition-client-library',
+      download_url='https://github.com/SAP/business-entity-recognition-client-library/archive/refs/heads/main.zip',
+      maintainer='Gokul Mohanarangan',
+      maintainer_email='gokumohan@gmail.com',
+      keywords=['business', 'entity', 'recognition', 'machine learning', 'SAP', 'client', 'library'],
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language :: Python :: 3'
+          ],
       )
